126e36866173a47002688301f10f31ca93a5d9e1
johnbangindc <johnbangindc@gmail.com>
2023-11-21 01:16:56 +0800

    2023-11-21 01:16:56

--- main.py
+++ main.py
@@ -24,3 +24,4 @@ if __name__ == "__main__":
 #include "../Share/fmtlib.h"
 template<typename... Args>
 inline void write_log(IParserSpi* sink, WTSLogLevel ll, const char* format, const Args&... args)
+{
