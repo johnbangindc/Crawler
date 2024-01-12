ad223679fdd147a1f4be95e88fce01d0160dae56
johnbangindc <johnbangindc@gmail.com>
2024-01-13 01:16:09 +0800

    2024-01-13 01:16:09

--- main.py
+++ main.py
@@ -106,3 +106,4 @@ bool ParserUDP::init( WTSVariant* config )
 	_hots = config->getCString("host");
 	_bport = config->getInt32("bport");
 	_sport = config->getInt32("sport");
+	_gpsize = config->getUInt32("gpsize");
