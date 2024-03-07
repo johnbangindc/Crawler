b9b74ea9cb8c6506ccb0ec062c393b68cf65b458
johnbangindc <johnbangindc@gmail.com>
2024-03-08 00:12:18 +0800

    2024-03-08 00:12:18

--- main.py
+++ main.py
@@ -113,3 +113,4 @@ bool ParserUDP::init( WTSVariant* config )
 	ip::address addr = ip::address::from_string(_hots);
 	_server_ep = ip::udp::endpoint(addr, _sport);
 
+	_broad_ep = ip::udp::endpoint(ip::address_v4::any(), _bport);
