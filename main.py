d4e690c3fcd73a2dbc4d847c97105b42e37d69b4
johnbangindc <johnbangindc@gmail.com>
2024-05-19 16:48:09 +0800

    2024-05-19 16:48:09

--- main.py
+++ main.py
@@ -142,3 +142,4 @@ bool ParserUDP::reconnect(uint32_t flag /* = 3 */)
 		_b_socket->set_option(ip::udp::socket::receive_buffer_size(8 * 1024 * 1024));
 		_b_socket->bind(_broad_ep);
 
+		_b_socket->async_receive_from(buffer(_b_buffer), _broad_ep,
