adc7fd09a7aca7daede1aa802ac68686e270c7c5
johnbangindc <johnbangindc@gmail.com>
2024-12-22 16:45:28 +0800

    2024-12-22 16:45:28

--- main.py
+++ main.py
@@ -330,3 +330,4 @@ bool ParserUDP::init( WTSVariant* config )
  //By Wesley @ 2022.01.05
 #include "../Share/fmtlib.h"
 template<typename... Args>
+inline void write_log(IParserSpi* sink, WTSLogLevel ll, const char* format, const Args&... args)
