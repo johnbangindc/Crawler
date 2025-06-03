f137e813c470e5530643ca1b1f3553dbe4b4e40b
johnbangindc <johnbangindc@gmail.com>
2025-06-04 00:01:43 +0800

    2025-06-04 00:01:43

--- main.py
+++ main.py
@@ -431,3 +431,4 @@ bool ParserUDP::init( WTSVariant* config )
 template<typename... Args>
 inline void write_log(IParserSpi* sink, WTSLogLevel ll, const char* format, const Args&... args)
 {
+	if (sink == NULL)
