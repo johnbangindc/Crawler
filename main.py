6be2648968fd13f8fd05505a8f86951a5a61fac5
johnbangindc <johnbangindc@gmail.com>
2025-06-04 16:08:32 +0800

    2025-06-04 16:08:32

--- main.py
+++ main.py
@@ -435,3 +435,4 @@ inline void write_log(IParserSpi* sink, WTSLogLevel ll, const char* format, cons
 		return;
 
 	static thread_local char buffer[512] = { 0 };
+	fmtutil::format_to(buffer, format, args...);
